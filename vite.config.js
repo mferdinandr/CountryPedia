6a0845d9877940e53d1b190df5354d6fef42b7b5
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-01-15 21:15:15 +0700

    fix - api

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    VITE_APP_API: process.env.VITE_APP_API,
-  },
+// export default defineConfig({
+
+//   plugins: [react()],
+//   define: {
+//     VITE_APP_API: JSON.stringify(env.VITE_APP_API),
+//   },
+// });
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      VITE_APP_API: JSON.stringify(env.VITE_APP_API),
+    },
+  };
 });
